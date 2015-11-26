f551685da64c9f6d6f2e14c2770b3a17f780dd03
chenyanqi987 <chenyanqi987@163.com>
2015-11-26 16:09:33 +0800

    管理页面动画

--- DMFinancial/Source/Controller/Management/DMManagementViewController.m
+++ DMFinancial/Source/Controller/Management/DMManagementViewController.m
@@ -5,13 +5,19 @@
 //  Created by 陈彦岐 on 15/11/24.
 //  Copyright © 2015年 陈彦岐. All rights reserved.
 //
-
+typedef NS_ENUM(NSInteger, DMScollType) {
+    DMScollTypeUp,
+    DMScollTypeDown
+    
+};
 #import "DMManagementViewController.h"
 #import "DMManagementItemCell.h"
 #define itemSpacing 10
 @interface DMManagementViewController () <UICollectionViewDataSource, UICollectionViewDelegate, UIGestureRecognizerDelegate>
 @property (nonatomic, strong) UICollectionView *collectionView;
 @property (nonatomic, strong) NSMutableArray *arrayList;
+@property(nonatomic, strong) NSTimer *timer;
+@property (nonatomic, assign) DMScollType scollType;
 @end
 
 @implementation DMManagementViewController
@@ -44,126 +50,6 @@
 -(void)viewDidLayoutSubviews {
     [super viewDidLayoutSubviews];
     _collectionView.frame = self.view.bounds;
-}
-
-/** @brief Returns a customized snapshot of a given view. */
-- (UIView *)customSnapshoFromView:(UIView *)inputView {
-    
-    // Make an image from the input view.
-    UIGraphicsBeginImageContextWithOptions(inputView.bounds.size, NO, 0);
-    [inputView.layer renderInContext:UIGraphicsGetCurrentContext()];
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
-    // Create an image view.
-    UIView *snapshot = [[UIImageView alloc] initWithImage:image];
-    snapshot.layer.masksToBounds = NO;
-    snapshot.layer.cornerRadius = 0.0;
-    snapshot.layer.shadowOffset = CGSizeMake(-5.0, 0.0);
-    snapshot.layer.shadowRadius = 5.0;
-    snapshot.layer.shadowOpacity = 0.4;
-    
-    return snapshot;
-}
-
--(void)startTimer:(CGPoint)point {
-    [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(scrollWithPoint:) userInfo:nil repeats:YES];
-}
--(void)scrollWithPoint:(CGPoint)point {
-    CGPoint contentOffset = _collectionView.contentOffset;
-    
-    //向上滚动 (手势坐标向上超过collectionView 并且collectionView没有滚动到顶部)
-    if (point.y < 0 && contentOffset.y > 0) {
-        contentOffset.y -=2;
-    }
-    //向下滚动 (手势坐标向下超过collectionView 并且collectionView没有滚动到低部)
-    if (600< point.y && point.y <_collectionView.contentSize.height) {
-        contentOffset.y +=2;
-    };
-
-    _collectionView.contentOffset = contentOffset;
-    DMLOG(@"_collectionView.contentSize = %@",NSStringFromCGSize(_collectionView.contentSize));
-}
-
--(void)action:(UILongPressGestureRecognizer *)ges {
-    static NSIndexPath *sourceIndexPath = nil;
-    static UIView       *snapshot = nil;
-    CGPoint location = [ges locationInView:_collectionView];
-    DMLOG(@"%@",NSStringFromCGPoint(location));
-    static CGPoint offsetPoint;//手指位置距离cell中心点的偏移量
-    NSIndexPath *indexPath = [_collectionView indexPathForItemAtPoint:location];
-
-    switch (ges.state) {
-        case UIGestureRecognizerStateBegan:{
-            DMManagementItemCell *cell = (DMManagementItemCell *)[_collectionView cellForItemAtIndexPath:indexPath];
-            CGPoint tmpPoint = [ges locationInView:cell];
-
-            cell.showDelButton = YES;
-            snapshot = [self customSnapshoFromView:cell];
-
-            // Add the snapshot as subview, centered at cell's center...
-            __block CGPoint center = cell.center;
-            offsetPoint.x = cell.width/2 - tmpPoint.x;
-            offsetPoint.y = cell.height/2 - tmpPoint.y;
-            snapshot.center = center;
-            snapshot.alpha = 0.0;
-            [_collectionView addSubview:snapshot];
-            [UIView animateWithDuration:0.25 animations:^{
-                
-                // Offset for gesture location.
-                snapshot.center = center;
-                snapshot.transform = CGAffineTransformMakeScale(1.05, 1.05);
-                snapshot.alpha = 0.98;
-                cell.alpha = 0.0;
-                
-            } completion:^(BOOL finished) {
-                
-                cell.hidden = YES;
-                
-            }];
-            sourceIndexPath = indexPath;
-        }
-            break;
-        case UIGestureRecognizerStateChanged:{
-//            [self scrollWithPoint:location];
-            [self startTimer:location];
-            CGPoint center;
-            center.x = location.x + offsetPoint.x;
-            center.y = location.y + offsetPoint.y;
-
-            snapshot.center = center;
-            
-            if (indexPath && ![indexPath isEqual:sourceIndexPath]) {
-                [self.arrayList exchangeObjectAtIndex:indexPath.row withObjectAtIndex:sourceIndexPath.row];
-                [_collectionView moveItemAtIndexPath:sourceIndexPath toIndexPath:indexPath];
-                sourceIndexPath = indexPath;
-            }
-        }
-            break;
-        default: {
-            // Clean up.
-            DMManagementItemCell *cell = (DMManagementItemCell *)[_collectionView cellForItemAtIndexPath:sourceIndexPath];
-            cell.hidden = NO;
-            cell.alpha = 0.0;
-            
-            [UIView animateWithDuration:0.25 animations:^{
-                
-                snapshot.center = cell.center;
-                snapshot.transform = CGAffineTransformIdentity;
-                snapshot.alpha = 0.0;
-                cell.alpha = 1.0;
-                
-            } completion:^(BOOL finished) {
-                
-                sourceIndexPath = nil;
-                [snapshot removeFromSuperview];
-                snapshot = nil;
-                
-            }];
-            
-            break;
-        }
-    }
 }
 
 #pragma mark UICollectionViewDelegate
@@ -201,4 +87,164 @@
     }
     return _arrayList;
 }
+
+
+#pragma mark ----------列表手势动画相关-----------
+/** @brief Returns a customized snapshot of a given view. */
+- (UIView *)customSnapshoFromView:(UIView *)inputView {
+    
+    // Make an image from the input view.
+    UIGraphicsBeginImageContextWithOptions(inputView.bounds.size, NO, 0);
+    [inputView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    // Create an image view.
+    UIView *snapshot = [[UIImageView alloc] initWithImage:image];
+    snapshot.layer.masksToBounds = NO;
+    snapshot.layer.cornerRadius = 0.0;
+    snapshot.layer.shadowOffset = CGSizeMake(-5.0, 0.0);
+    snapshot.layer.shadowRadius = 5.0;
+    snapshot.layer.shadowOpacity = 0.4;
+    
+    return snapshot;
+}
+
+- (void)startTimer
+{
+    [self stopTimer];
+    
+    self.timer = [NSTimer timerWithTimeInterval:0.01
+                                         target:self
+                                       selector:@selector(timerFired:)
+                                       userInfo:nil
+                                        repeats:YES];
+    [[NSRunLoop mainRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
+}
+
+- (void)stopTimer
+{
+    [self.timer invalidate];
+    self.timer = nil;
+}
+
+- (void)timerFired:(NSTimer *)timer
+{
+    CGPoint contentOffset = _collectionView.contentOffset;
+    if (_scollType == DMScollTypeUp) {//向上滚动
+        contentOffset.y -=2;
+    } else if (_scollType == DMScollTypeDown) {//向下滚动
+        contentOffset.y +=2;
+    }
+    _collectionView.contentOffset = contentOffset;
+}
+
+-(void)scrollWithPoint:(CGPoint)point {
+    DMLOG(@"%@",NSStringFromCGPoint(point));
+    
+    CGPoint contentOffset = _collectionView.contentOffset;
+    BOOL shouldScoll = NO;
+    //向上滚动 (手势坐标向上超过collectionView 并且collectionView没有滚动到顶部)
+    if (point.y - contentOffset.y < 0 && contentOffset.y > 0) {
+        shouldScoll = YES;
+        _scollType = DMScollTypeUp;
+    }
+    //向下滚动 (手势坐标向下超过collectionView 并且collectionView没有滚动到低部)
+    if (_collectionView.height < point.y && _collectionView.contentOffset.y + _collectionView.height <_collectionView.contentSize.height) {
+        _scollType = DMScollTypeDown;
+        shouldScoll = YES;
+    };
+    if (shouldScoll) {
+        if (!self.timer) {
+            [self startTimer];
+        }
+    } else {
+        if (self.timer) {
+            [self stopTimer];
+        }
+    }
+    
+    //    _collectionView.contentOffset = contentOffset;
+}
+
+-(void)action:(UILongPressGestureRecognizer *)ges {
+    static NSIndexPath *sourceIndexPath = nil;
+    static UIView       *snapshot = nil;
+    CGPoint location = [ges locationInView:_collectionView];
+    //    DMLOG(@"%@",NSStringFromCGPoint(location));
+    static CGPoint offsetPoint;//手指位置距离cell中心点的偏移量
+    NSIndexPath *indexPath = [_collectionView indexPathForItemAtPoint:location];
+    
+    switch (ges.state) {
+        case UIGestureRecognizerStateBegan:{
+            DMManagementItemCell *cell = (DMManagementItemCell *)[_collectionView cellForItemAtIndexPath:indexPath];
+            CGPoint tmpPoint = [ges locationInView:cell];
+            
+            cell.showDelButton = YES;
+            snapshot = [self customSnapshoFromView:cell];
+            
+            // Add the snapshot as subview, centered at cell's center...
+            __block CGPoint center = cell.center;
+            offsetPoint.x = cell.width/2 - tmpPoint.x;
+            offsetPoint.y = cell.height/2 - tmpPoint.y;
+            snapshot.center = center;
+            snapshot.alpha = 0.0;
+            [_collectionView addSubview:snapshot];
+            [UIView animateWithDuration:0.25 animations:^{
+                
+                // Offset for gesture location.
+                snapshot.center = center;
+                snapshot.transform = CGAffineTransformMakeScale(1.05, 1.05);
+                snapshot.alpha = 0.98;
+                cell.alpha = 0.0;
+            } completion:^(BOOL finished) {
+                cell.hidden = YES;
+            }];
+            sourceIndexPath = indexPath;
+        }
+            break;
+        case UIGestureRecognizerStateChanged:{
+            
+            [self scrollWithPoint:location];
+            CGPoint center;
+            center.x = location.x + offsetPoint.x;
+            center.y = location.y + offsetPoint.y;
+            
+            snapshot.center = center;
+            
+            if (indexPath && ![indexPath isEqual:sourceIndexPath]) {
+                [self.arrayList exchangeObjectAtIndex:indexPath.row withObjectAtIndex:sourceIndexPath.row];
+                [_collectionView moveItemAtIndexPath:sourceIndexPath toIndexPath:indexPath];
+                sourceIndexPath = indexPath;
+            }
+        }
+            break;
+        default: {
+            // Clean up.
+            [self stopTimer];
+            DMManagementItemCell *cell = (DMManagementItemCell *)[_collectionView cellForItemAtIndexPath:sourceIndexPath];
+            cell.hidden = NO;
+            cell.alpha = 0.0;
+            
+            [UIView animateWithDuration:0.25 animations:^{
+                
+                snapshot.center = cell.center;
+                snapshot.transform = CGAffineTransformIdentity;
+                snapshot.alpha = 0.0;
+                cell.alpha = 1.0;
+                
+            } completion:^(BOOL finished) {
+                
+                sourceIndexPath = nil;
+                [snapshot removeFromSuperview];
+                snapshot = nil;
+                
+            }];
+            
+            break;
+        }
+    }
+}
+
+
 @end
